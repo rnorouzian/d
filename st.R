d61b232ceb3e482f1d05f7b5f5f4a15a032a6943
Reza Norouzian <rnorouzian@gmail.com>
2020-10-06 02:15:03 -0500

    Update st.R

--- st.R
+++ st.R
@@ -1,17 +1,21 @@
+need <- c("tidyverse", "survey", "sampling")
+not.have <- need[!(need %in% installed.packages()[,"Package"])]
+if(length(not.have)) install.packages(not.have)
+
 library(tidyverse)
 library(survey)
+library(sampling)
 
 prop_str_sample <- function(sample_frame, vars = NULL, n = 10, seed = NULL){
   
   if(!inherits(sample_frame, c("data.frame", "tibble", "matrix"))) stop("sample_frame must be a data.frame.", call. = FALSE)   
   if(is.null(vars)) vars <- names(sample_frame)
   
-  
   DATA1 <- sample_frame %>%
     group_by(across(all_of(vars))) %>%
     summarise(n = n(), .groups = 'drop') %>%
     mutate(fpc = n/sum(n)) %>% 
-    right_join(sample_frame)
+    right_join(sample_frame, by = vars)
   
   
   set.seed(seed)
@@ -33,15 +37,17 @@ prop_str_sample <- function(sample_frame, vars = NULL, n = 10, seed = NULL){
     group_by(across(all_of(vars))) %>%
     summarise(n = n(), .groups = 'drop') %>%
     mutate(fpc = n/sum(n)) %>% 
-    right_join(DF)
+    right_join(DF, by = vars)
   
-DF <- left_join(DATA1, DF, by = vars) %>%  
-    mutate(fpc = coalesce(fpc.y, fpc.x))  ## Error: Can't subset columns that don't exist. @@@@@@@@@@@@@@@@@@@@@
-                                                                ## x Columns `n`, `fake.name`, `sector`, `pretest`, `state`, etc. don't exist. @@@@@@@
+  DF <- DF %>%
+    left_join(DATA1 %>% distinct(across(all_of(vars)), fpc), 
+              by = vars) %>%
+    mutate(fpc = coalesce(fpc.y, fpc.x)) %>%
+    select(names(DF)) %>% as.data.frame()
   
   return(list(frame_strata_freq = addmargins(f_freq), frame_strata_prop = round(addmargins(prop.table(f_freq)),3), sample_strata_prop = round(addmargins(prop.table(s_freq)),3), sample_strata_freq=addmargins(s_freq), data = DF))
 }
 
 d <- read.csv('https://raw.githubusercontent.com/rnorouzian/d/master/su.csv')
 
-(out <- prop_str_sample(d, c("gender", "pre"), n=200))
+#(out <- prop_str_sample(d, c("gender", "pre"), n=200))
