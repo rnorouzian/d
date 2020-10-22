114b52c0ef97b285e074b3788e6524da6372306e
Reza Norouzian <rnorouzian@gmail.com>
2020-10-21 19:08:04 -0500

    Update cost_power.R

--- cost_power.R
+++ cost_power.R
@@ -1,79 +1,85 @@
-cost_crt <- function(
+cost_CRT <- function(
   
-ctreat = 200 ,
-streat = 10 ,
-ccon = 100 ,
-scon = 10 ,
-iccmax = 0.1 ,
-Vmax = 2,                      ## How can a user know what to input for 'Vmax' & 'mean_dif'?
-SDratiomax = 1 ,
-SDratiomin = 1/SDratiomax,
-mean_dif = 0.5,               ## I changed beta_1 to 'mean_dif'
-power = 80)
+  cost_per_c_treat = 200 ,
+  cost_per_s_treat = 15 ,
+  cost_per_c_cont = 100 ,
+  cost_per_s_cont = 10 ,
+  iccmax = 0.15 ,
+  mdes = .25,                      
+  SDtreat_to_cont = 1.2 ,
+  SDratiomin = 1/SDtreat_to_cont,
+  power = 80,
+  tail = 2 )
 {
-
-  alpha = 5  
-  tail = 2 
   
-ESmin <- mean_dif/sqrt(Vmax/2)
-zbeta <- qnorm((power/100),mean = 0, sd =1)
-zalpha <- qnorm(1-(alpha/(100*tail)),mean = 0, sd =1)
-maxvarmean_difhat <- (mean_dif / (zbeta + zalpha))**2   # 'mean_dif' is also used here
-
-
-ntreat <- sqrt((ctreat/streat)*((1-iccmax)/iccmax))
-ncon <- sqrt((ccon/scon)*((1-iccmax)/iccmax))
-costpertreatcluster <- ctreat + (streat*ntreat)
-costperconcluster <- ccon + (scon*ncon)
-gtreat <- (sqrt(ctreat*iccmax) + sqrt(streat*(1-iccmax)))**2
-gcon <- (sqrt(ccon*iccmax) + sqrt(scon*(1-iccmax)))**2
-pratio <- sqrt(gtreat/gcon)
-
-
-budgetratio <- 99999
-budgetratio <- ifelse( ((pratio <= SDratiomax) & (pratio >= SDratiomin)), pratio**2, ifelse((pratio > SDratiomax), pratio*SDratiomax, pratio*SDratiomin))
-fraction <- budgetratio/(1 + budgetratio)
-
-mmvnumer <- 99999
-mmvnumer <- ifelse( ((pratio <= SDratiomax) & (pratio >= SDratiomin)),
-                    gcon*Vmax*(1+(pratio**2)),                                #Varmax is used here as well
-                    ifelse((pratio > SDratiomax),
-                           gcon*Vmax*(((pratio*SDratiomax)+1)**2/((SDratiomax**2)+1)),
-                           gcon*Vmax*(((pratio*SDratiomin)+1)**2/((SDratiomin**2) + 1))   ) )
-
-
-budget <- mmvnumer/maxvarmean_difhat
-treatbudget <- fraction*budget
-conbudget <- (1-fraction)*budget
-ktreat <- treatbudget/costpertreatcluster
-kcon <- conbudget/costperconcluster
-
-
-ktreatrup <- ceiling(ktreat)
-kconrup <- ceiling(kcon)
-
-ktreatplus <- ifelse(alpha == 5, 
-                      ifelse (pmin(ktreatrup,kconrup) < 8, 
-                              ktreatrup + 3, ktreatrup + 2), 
-                      ifelse( (alpha == 1), ktreatrup + 4, 99999)) 
-
-kconplus <- ifelse(alpha == 5, 
-                    ifelse (pmin(ktreatrup,kconrup) < 8, 
-                            kconrup + 3, kconrup + 2) , 
-                    ifelse(alpha == 1, kconrup + 4,  99999)) 
-
-budgetplus <- (ktreatplus*costpertreatcluster) + 
-  (kconplus*costperconcluster)
-
-message("
-     ncon: sample size per control cluster
-   ntreat: sample size per treated cluster
-   ktreat: # of treated clusters
-     kcon: # of control clusters
-   budget: total budget needed\n")
-
-data.frame(SE_effect = sqrt(maxvarmean_difhat),ncon, ntreat, ktreat = ktreatplus,kcon=kconplus,budget=paste0("$",round(budgetplus, 2)), row.names = "STUDY:")
+alpha <- 5
+  
+inv_d <- function(mdes) {
+  c(mean_dif = 1, Vmax = 2/mdes^2)
 }
 
-# EXAMPLE OF USE:
-#cost_crt(iccmax = .2, power = 80, Vmax = 3)
+  pars <- inv_d(mdes)
+  mean_dif <- pars[[1]]
+  Vmax <- pars[[2]]
+  zbeta <- qnorm((power/100))
+  zalpha <- qnorm(1-(alpha/(100*tail)))
+  maxvarmean_difhat <- (mean_dif / (zbeta + zalpha))**2  
+  
+  
+  qnorm(1-(5/(100*1)),mean = 0, sd =1)
+  
+  
+  ntreat <- sqrt((cost_per_c_treat/cost_per_s_treat)*((1-iccmax)/iccmax))
+  ncont <- sqrt((cost_per_c_cont/cost_per_s_cont)*((1-iccmax)/iccmax))
+  costpertreatcluster <- cost_per_c_treat + (cost_per_s_treat*ntreat)
+  costperconcluster <- cost_per_c_cont + (cost_per_s_cont*ncont)
+  gtreat <- (sqrt(cost_per_c_treat*iccmax) + sqrt(cost_per_s_treat*(1-iccmax)))**2
+  gcon <- (sqrt(cost_per_c_cont*iccmax) + sqrt(cost_per_s_cont*(1-iccmax)))**2
+  pratio <- sqrt(gtreat/gcon)
+  
+  
+  budgetratio <- 99999
+  budgetratio <- ifelse( ((pratio <= SDtreat_to_cont) & (pratio >= SDratiomin)), pratio**2, ifelse((pratio > SDtreat_to_cont), pratio*SDtreat_to_cont, pratio*SDratiomin))
+  fraction <- budgetratio/(1 + budgetratio)
+  
+  mmvnumer <- 99999
+  mmvnumer <- ifelse( ((pratio <= SDtreat_to_cont) & (pratio >= SDratiomin)),
+                      gcon*Vmax*(1+(pratio**2)),                                #Varmax is used here as well
+                      ifelse((pratio > SDtreat_to_cont),
+                             gcon*Vmax*(((pratio*SDtreat_to_cont)+1)**2/((SDtreat_to_cont**2)+1)),
+                             gcon*Vmax*(((pratio*SDratiomin)+1)**2/((SDratiomin**2) + 1))   ) )
+  
+  
+  budget <- mmvnumer/maxvarmean_difhat
+  treatbudget <- fraction*budget
+  conbudget <- (1-fraction)*budget
+  ktreat <- treatbudget/costpertreatcluster
+  kcont <- conbudget/costperconcluster
+  
+  
+  ktreatrup <- ceiling(ktreat)
+  kcontrup <- ceiling(kcont)
+  
+  ktreatplus <- ifelse(alpha == 5, 
+                       ifelse (pmin(ktreatrup,kcontrup) < 8, 
+                               ktreatrup + 3, ktreatrup + 2), 
+                       ifelse( (alpha == 1), ktreatrup + 4, 99999)) 
+  
+  kcontplus <- ifelse(alpha == 5, 
+                     ifelse (pmin(ktreatrup,kcontrup) < 8, 
+                             kcontrup + 3, kcontrup + 2) , 
+                     ifelse(alpha == 1, kcontrup + 4,  99999)) 
+  
+  budgetplus <- (ktreatplus*costpertreatcluster) + 
+    (kcontplus*costperconcluster)
+  
+  message("
+    ncont: sample size per control cluster
+   ntreat: sample size per treated cluster
+   ktreat: # of treated clusters
+    kcont: # of control clusters
+   budget: total budget needed\n")
+  
+  data.frame(mdes = mdes, ncont = ceiling(ncont), ntreat = ceiling(ntreat), ktreat = ktreatplus, kcont = kcontplus, budget = paste0("$", round(budgetplus, 2)), 
+             power = paste0(power, "%"), row.names = "STUDY:")
+}
