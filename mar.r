1cf3c28a172296990b09cd8fa9fce312025da1c9
Reza Norouzian <rnorouzian@gmail.com>
2020-11-10 20:25:35 -0600

    Update mar.r

--- mar.r
+++ mar.r
@@ -7,7 +7,10 @@ library(RBtest)
 
 mar_mcar <- function(data){
   
-  res <- suppressWarnings(RBtest::RBtest.iter(data, 5)$type.final) 
-  noquote(ifelse(res == 0, "MCAR", ifelse(res == 1, "MAR", "NA")))
+  res <- try(suppressWarnings(RBtest::RBtest.iter(data, 5)$type.final), silent = TRUE) 
+  
+  if(inherits(res, "try-error")) res <- suppressWarnings(RBtest::RBtest(data)$type)
+  
+  noquote(ifelse(res == 0, "MCAR", ifelse(res == 1, "MAR", NA)))
   
 }
