564250d20919b81412889a61650538423a286a20
Reza Norouzian <rnorouzian@gmail.com>
2021-01-16 19:41:29 -0600

    Add files via upload

---
+++ d.r
@@ -0,0 +1,16 @@
+needxzc <- c("tidyverse", "survey", "sampling", "randomizr", "psych", "RBtest", "mice", "BiocManager", "DAKS")
+not.have <- needxzc[!(needxzc %in% installed.packages()[,"Package"])]
+if(length(not.have)) install.packages(not.have)
+
+
+suppressWarnings(                                         
+  suppressMessages({ 
+    
+    for(i in needxzc){
+      library(i, character.only = TRUE)
+    }
+  })) 
+
+source('https://raw.githubusercontent.com/rnorouzian/d/master/Design.R')
+
+source('https://raw.githubusercontent.com/rnorouzian/d/master/cost_power.R')
